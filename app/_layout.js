f252b3f9309361dfd276b2b9d1a2edef06b97d30
Charles <charles42.h@gmail.com>
2024-07-29 21:17:06 +0200

    Create createNote page

--- app/_layout.js
+++ app/_layout.js
@@ -1,13 +1,22 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 
 import Feather from '@expo/vector-icons/Feather';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen
       name="index"
-      />
+      options={{ title: "",
+      headerRight: () => (
+        <Link href="createNote">
+        <Feather
+          name="edit"
+          size={24}
+        />
+        </Link>
+       )}}/>
       <Stack.Screen
       name="note"
       options={{ title: "",
@@ -16,6 +25,15 @@ export default function RootLayout() {
           name="edit"
           size={24}
         />),
+      }} />
+      <Stack.Screen
+      name="createNote"
+      options={{ title: "",
+      headerRight: () => (
+        <FontAwesome6
+          name="check"
+          size={24}
+          color="black" />),
       }} />
     </Stack>
   );
---
+++ app/createNote.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+
+import BaseLayout from '../components/BaseLayout'
+
+
+export default function createNote() {
+  return (
+    <BaseLayout>
+      <View>
+        <Text>createNote</Text>
+      </View>
+    </BaseLayout>
+  )
+}
