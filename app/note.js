baecf10287d9b9b5abe1ff9b27b581b9e5588491
Charles <charles42.h@gmail.com>
2024-08-01 10:41:38 +0200

    Handle deletion of note in note page

--- app/note.js
+++ app/note.js
@@ -1,17 +1,35 @@
 import { useState, useEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import { useLocalSearchParams, router } from "expo-router";
 
 import { Entypo } from '@expo/vector-icons';
 
 import BaseLayout from "../components/BaseLayout";
 import PriorityIndicator from "../components/PriorityIndicator";
 
+import { getData, storeData } from '../hooks/useAsyncData';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 
 export default function Note() {
   const rawNoteData = useLocalSearchParams();
 
   const [noteData, setNoteData] = useState(null);
+
+  const handleNoteDeletion = async () => {
+    getData('myNotesData')
+    .then((data) => {
+      const updatedData = data.filter(note => note.id !== noteData.id);
+      storeData('myNotesData', updatedData);
+    })
+    .then(() => {
+      router.back();
+    })
+    .catch((e) => {
+      alert("Error deleting note");
+    });
+  };
 
   useEffect(() => {
     setNoteData(sanitizeNoteData(rawNoteData));
@@ -28,23 +46,32 @@ export default function Note() {
     <BaseLayout>
       {noteData &&
         <View>
-          <Text style={styles.noteTitle}>{noteData.title}</Text>
-          <View style={styles.noteInfo}>
-            <Text style={styles.noteInfoData}>{noteData.creationDate}</Text>
-            {
-              noteData.modificationDate !== "null" &&
-              <View style={styles.rowCenter}>
-                <View style={styles.rowCenter}>
-                  <Entypo name="dot-single" style={styles.modifiedDot} size={24} color="grey" />
-                  <Text style={styles.noteInfoData}>modified</Text>
-                </View>
-                <Text style={styles.noteInfoData}>{noteData.modificationDate}</Text>
+          <View style={styles.noteHeader}>
+            <View style={styles.noteAllInfoContainer}>
+              <Text style={styles.noteTitle}>{noteData.title}</Text>
+              <View style={styles.noteInfo}>
+                <Text style={styles.noteInfoData}>{noteData.creationDate}</Text>
+                {
+                  noteData.modificationDate !== "null" &&
+                  <View style={styles.rowCenter}>
+                    <View style={styles.rowCenter}>
+                      <Entypo name="dot-single" style={styles.modifiedDot} size={24} color="grey" />
+                      <Text style={styles.noteInfoData}>modified</Text>
+                    </View>
+                    <Text style={styles.noteInfoData}>{noteData.modificationDate}</Text>
+                  </View>
+                }
+                {
+                  noteData.priority &&
+                  <PriorityIndicator style={styles.noteInfoData} priority={noteData.priority} />
+                }
               </View>
-            }
-            {
-              noteData.priority &&
-              <PriorityIndicator style={styles.noteInfoData} priority={noteData.priority} />
-            }
+            </View>
+            <View style={styles.deleteNoteBtnContainer}>
+              <Pressable style={styles.deleteNoteBtn} onPress={() => handleNoteDeletion()}>
+                <FontAwesome name="trash-o" size={24} color="black" />
+            </Pressable>
+            </View>
           </View>
           <View>
             <Text>
@@ -58,6 +85,17 @@ export default function Note() {
 }
 
 const styles = StyleSheet.create({
+  noteHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#E0E0E0',
+    paddingBottom: 5,
+    marginBottom: 10,
+  },
+  noteAllInfoContainer: {
+    flex: 1,
+  },
   noteTitle: {
     fontSize: 30,
     fontWeight: 'bold',
@@ -66,10 +104,6 @@ const styles = StyleSheet.create({
     display: 'inline-block',
     flexDirection: 'row',
     alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#E0E0E0',
-    paddingVertical: 5,
-    marginBottom: 10,
   },
   rowCenter: {
     flexDirection: 'row',
@@ -81,5 +115,12 @@ const styles = StyleSheet.create({
   modifiedDot: {
     marginStart: -10,
     marginEnd: -3,
+  },
+  deleteNoteBtn: {
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+    backgroundColor: '#F1F0F0',
+    padding: 10,
+    borderRadius: 5,
   },
 });
